5b5a24673894bfcd072315d6dbc6da57b1168188
Zaara <hafsahaqs@gmail.com>
2023-10-17 01:24:16 -0400

    add calender ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Cal from "./pages/Cal";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
   return (
     <div>
-      <h1 className="ml-4 flex h-24 border-2 border-gray-300 p-3 text-gray-700 shadow-md"></h1>
+      <Cal />
     </div>
   );
 }
